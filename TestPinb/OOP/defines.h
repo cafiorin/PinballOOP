0c8abf64da8e9cfd91040e7404f09db9a046d871
c.fiorin <c.fiorin@samsung.com>
2017-08-15 08:51:55 -0300

    Created OutBall

--- TestPinb/OOP/defines.h
+++ TestPinb/OOP/defines.h
@@ -13,6 +13,69 @@ http://pinballhomemade.blogspot.com.br
 #define DEBUGMESSAGES
 //#define DEBUGMESSAGESLOOP
 
+//Inputs
+#define I1 1
+#define I2 2
+#define I3 3
+#define I3ALT 39
+#define I4 4
+#define I4ALT 38
+#define I5 5
+#define I6 6
+#define I7 7
+#define I8 8
+#define I9 9
+#define I10 10
+#define I11 11
+#define I12 12
+#define I13 13
+#define I14 14
+#define I15 15
+#define I16 16
+#define I17 17
+#define I18 18
+#define I19 19
+#define I20 20
+#define I21 21
+#define I22 22
+#define I23 23
+#define I24 24
+#define I25 25
+#define I26 26
+#define I27 27
+#define I28 28
+#define I29 29
+#define I30 30
+#define I31 31
+#define I32 32
+#define I33 33
+#define I34 34
+#define I35 35
+#define I36 36
+#define I37 37
+#define I38 38
+#define I39 39
+#define I40 40
+#define I41 41
+
+//OutPut COIN
+#define O0 13
+#define O1 1
+#define O2 2
+#define O3 3
+#define O4 4
+#define O5 5
+#define O6 6
+#define O7 7
+#define O8 8
+#define O9 9
+#define O10 10
+#define O11 11
+#define O12 12
+
+
+
+
 #define DOS
 //#define ARDUINO
 
--- TestPinb/TestPinb.cpp
+++ TestPinb/TestPinb.cpp
@@ -18,24 +18,68 @@ http://pinballhomemade.blogspot.com.br
 #include "OOP\SlingShot.h"
 #include "OOP\Bumper.h"
 #include "OOP\KickoutHole.h"
+#include "OOP\DropTarget.h"
+#include "OOP\OutBall.h"
 #include "OOP\Utils.h"
 
 void InitObjectsToPinballMaster(Pinball *pPinball)
 {
-	PinballObject *pInput1 = new Input("input1", pPinball, 1);
+	Output *pTurnFlipperOn = new Output("TFO", pPinball, O0);
 
-	Output *pOutput = new Output("output", pPinball, 2);
+	OutBall *pOutBall = new OutBall("OB", pPinball, I1, O1, I2, O2);
+	SlingShot *pSlingShotLeft = new SlingShot("SLL", pPinball, I3, I3ALT, O3);
+	SlingShot *pSlingShotRight = new SlingShot("SLR", pPinball, I4, I4ALT, O4);
 
-	SlingShot *pSling = new SlingShot("sling", pPinball, 3, 4, 5);
-	Bumper *pBumper = new Bumper("bumper", pPinball, 6, 7);
-	KickoutHole *pHole = new KickoutHole("hole", pPinball, 8, 9);
+	Input *pInputOutLaneLeft = new Input("OLL", pPinball, I5);
+	Input *pInputReturnBallLeft  = new Input("RBL", pPinball, I6);
+	Input *pInputReturnBallRight = new Input("RBR", pPinball, I7);
+	Input *pInputOutLaneRight = new Input("OLR", pPinball, I8);
 
-	pOutput->TurnOnByTimer(500);
+	Input *pInputTargetGreen1 = new Input("TG1", pPinball, I9);
+	Input *pInputTargetRed1   = new Input("TR1", pPinball, I10);
+
+	DropTarget *pDropTarget5 = new DropTarget("DT5", pPinball, I11,I12,I13,I14,I15,O5);
+	Input *pInputRolloverStarRed1 = new Input("RSR1", pPinball, I16);
+	DropTarget *pDropTarget3 = new DropTarget("DT3", pPinball, I17, I18, I19, O6);
+
+	Input *pInputTargetRed2    = new Input("TR2", pPinball, I20);
+	Input *pInputTargetYellow2 = new Input("TY2", pPinball, I21);
+	Input *pInputTargetGreen2  = new Input("TG2", pPinball, I22);
+
+	Input *pInputTargetYellow1 = new Input("TY1", pPinball, I23);
+
+	KickoutHole *pHole    = new KickoutHole("HOLE", pPinball, I24, O7);
+	Bumper *pBumperLeft   = new Bumper("BL", pPinball, I25, O8);
+	Bumper *pBumperCenter = new Bumper("BC", pPinball, I26, O9);
+	Bumper *pBumperRight  = new Bumper("BR", pPinball, I27, O10);
+
+	Input *pInputRolloverStarGreen = new Input("RSG", pPinball, I28);
+
+	Input *pInputRolloverLeft   = new Input("RML", pPinball, I29);
+	Input *pInputRolloverCenter = new Input("RMC", pPinball, I30);
+	Input *pInputRolloverRight  = new Input("RMR", pPinball, I31);
+
+	Input *pInputRolloverStarRed2 = new Input("RSR2", pPinball, I32);
+
+	Input *pInputTargetHigher = new Input("TH", pPinball, I33);
+
+	Input *pInputSpinner = new Input("Spin", pPinball, I37);
 }
 
 void InitObjectsToPinballSlave(Pinball *pPinball)
 {
-	SlingShot *pSling = new SlingShot("sling2", pPinball, 1, 2, 3);
+	Input *pInputDropTarget31 = new Input("DT31", pPinball, I17);
+	Input *pInputDropTarget32 = new Input("DT32", pPinball, I18);
+	Input *pInputDropTarget33 = new Input("DT33", pPinball, I19);
+
+	Input *pInputRampIn = new Input("RampIn", pPinball, I34);
+	Input *pInputRampOut1 = new Input("RampO1", pPinball, I35);
+	Input *pInputRampOut2 = new Input("RampO2", pPinball, I36);
+
+	Input *pInputStartButton = new Input("SB", pPinball, I38);
+	Input *pInputMenu = new Input("BM", pPinball, I39);
+	Input *pInputVolumePlus = new Input("VP", pPinball, I40);
+	Input *pInputVolumeMinus = new Input("VM", pPinball, I41);
 }
 
 int ikeyCount = 0;

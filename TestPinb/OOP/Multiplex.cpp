8a7009cf2441cf97e38e5b7f3ecea24ac4344cfc
cafiorin@gmail.com <cafiorin@gmail.com>
2017-10-24 13:23:05 -0200

    changed multiplex

--- TestPinb/OOP/Multiplex.cpp
+++ TestPinb/OOP/Multiplex.cpp
@@ -22,17 +22,20 @@ static const uint8_t _muxChAddress[16][4] = {
     {1,1,1,1}  //channel 15
 };
 
-Multiplex::Multiplex(const uint8_t S0,const uint8_t S1,const uint8_t S2,const uint8_t S3,const uint8_t SIG, const uint8_t chipSelect1, const uint8_t chipSelect2, const uint8_t chipSelect3)
+Multiplex::Multiplex(const uint8_t S0,const uint8_t S1,const uint8_t S2,const uint8_t S3,const uint8_t SIG, const uint8_t OUTSIG, const uint8_t chipSelect1, const uint8_t chipSelect2, const uint8_t chipSelect3, const uint8_t chipSelect4, const uint8_t chipSelect5)
 {
 	_adrsPin[0] = S0;
 	_adrsPin[1] = S1;
 	_adrsPin[2] = S2;
 	_adrsPin[3] = S3;
 	_sig = SIG;
+	_outsig = OUTSIG;
 
 	_chipSelect1 = chipSelect1;
 	_chipSelect2 = chipSelect2;
 	_chipSelect3 = chipSelect3;
+	_chipSelect4 = chipSelect4;
+	_chipSelect5 = chipSelect5;
 }
 
 void Multiplex::enableChip(uint8_t chipNumber)
@@ -41,14 +44,19 @@ void Multiplex::enableChip(uint8_t chipNumber)
 	digitalWrite(_chipSelect1, HIGH);
 	digitalWrite(_chipSelect2, HIGH);
 	digitalWrite(_chipSelect3, HIGH);
+	digitalWrite(_chipSelect4, HIGH);
+	digitalWrite(_chipSelect5, HIGH);
 
 	//Enable only one
 	digitalWrite(chipNumber, LOW);
 }
 
-void Multiplex::begin()
+void Multiplex::init()
 {
 	uint8_t i;
+
+	pinMode(_sig, INPUT);
+	pinMode(_outsig, OUTPUT);
 
 	pinMode(_chipSelect1, OUTPUT);
 	digitalWrite(_chipSelect1, HIGH);
@@ -59,24 +67,74 @@ void Multiplex::begin()
 	pinMode(_chipSelect3, OUTPUT);
 	digitalWrite(_chipSelect3, HIGH);
 
+	pinMode(_chipSelect4, OUTPUT);
+	digitalWrite(_chipSelect4, HIGH);
+
+	pinMode(_chipSelect5, OUTPUT);
+	digitalWrite(_chipSelect5, HIGH);
+
 	for (i = 0; i < 4; i++)
 	{
 		pinMode(_adrsPin[i], OUTPUT); 
 		digitalWrite(_adrsPin[i],LOW);
 	}
+
 }
+
+void Multiplex::resetAllOutput()
+{
+	digitalWrite(_outsig, LOW);
+
+	digitalWrite(_chipSelect4, LOW);
+	digitalWrite(_chipSelect5, LOW);
+
+	for (i = 0; i < 16; i++)
+	{
+		_addressing(ch);
+	}
+
+	digitalWrite(_chipSelect4, HIGH);
+	digitalWrite(_chipSelect5, HIGH);
+}
+
+
+void Multiplex::writeChannel(bool enableChip4, uint8_t ch,int value)
+{
+	if (ch < 0 || ch > 15)
+		return 0;
+
+	digitalWrite(_outsig, value);
+	_addressing(ch);
+
+	if(enableChip4)
+		enableChip(_chipSelect4);
+	else
+		enableChip(_chipSelect5);
+}
+
 
 int Multiplex::readChannel(uint8_t ch)
 {
 	if (ch < 0 || ch > 47)
 		return 0;
 
-	if (ch < 16) enableChip(_chipSelect1);
-	else if (ch >= 16 && ch < 32) enableChip(_chipSelect2);
-	else if (ch >=32) enableChip(_chipSelect3);
+	if (ch < 16)
+	{
+		enableChip(_chipSelect1);
+	}
+	else if (ch >= 16 && ch < 32)
+	{
+		ch -= 16;
+		enableChip(_chipSelect2);
+	}
+	else if (ch >= 32)
+	{
+		ch -= 32;
+		enableChip(_chipSelect3);
+	}
 
 	_addressing(ch);
-	return analogRead(_sig);
+	return digitalRead(_sig);
 }
 
 
--- TestPinb/OOP/Multiplex.h
+++ TestPinb/OOP/Multiplex.h
@@ -18,6 +18,7 @@
 	void loop()
 	{
 	  plex.readChannel(0); //it read the channel 0 from chip 1
+	  plex.writeChannel(3,LOW);//write LOW to channel 3
 	}
 
 */
@@ -31,20 +32,26 @@
 class Multiplex 
 {
  public:
-	 Multiplex(const uint8_t S0,const uint8_t S1,const uint8_t S2,const uint8_t S3,const uint8_t SIG,const uint8_t chipSelect1, const uint8_t chipSelect2, const uint8_t chipSelect3);
-	 void 			begin();
-	 int 			readChannel(uint8_t ch);
+	 Multiplex(const uint8_t S0, const uint8_t S1, const uint8_t S2, const uint8_t S3, const uint8_t SIG, , const uint8_t OUTSIG, const uint8_t chipSelect1, const uint8_t chipSelect2, const uint8_t chipSelect3, const uint8_t chipSelect4, const uint8_t chipSelect5);
+	 void 			init();
 	 void			enableChip(uint8_t chipNumber);
+	 int 			readChannel(uint8_t ch);
+	 void			writeChannel(bool enableChip4, uint8_t ch, int value);
+	 void			resetAllOutput();
 
  private:
 	 uint8_t	 _adrsPin[4];
 	 uint8_t 	_sig;
-	 void 		_addressing(uint8_t ch);
+	 uint8_t 	_outsig;
+
 	 uint8_t 	_chipSelect1;
 	 uint8_t 	_chipSelect2;
 	 uint8_t 	_chipSelect3;
+	 uint8_t 	_chipSelect4;
+	 uint8_t 	_chipSelect5;
 
- 
+	 void 		_addressing(uint8_t ch);
+
 };
 #endif
 

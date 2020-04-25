2360e30cfe304eaaf599078e9ba947f1f157debf
Cassius <c.fiorin@dmz.org.br>
2020-04-25 00:48:23 -0300

    Create a new class PinballMachine

---
+++ TestPinb/PinballMachine.h
@@ -0,0 +1,17 @@
+#ifndef PinballMachine__INCLUDED_
+#define PinballMachine__INCLUDED_
+
+class PinballMachine
+{
+	//Porta com botoes
+	//Display
+	//Playfield
+	//lista de Jogadores (Max 8)
+	//Som
+	
+	//Display debug e botoes debug
+	//Saida serial debug
+
+};
+
+#endif
